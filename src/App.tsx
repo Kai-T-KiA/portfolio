91b3a39b208d4046a4f0cd7fd8805bb7a17484b4
竹内 快 <>
2024-09-16 23:48:12 +0900

    About.tsxの作成と表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header/header';
+import About from './components/about/about';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <About />
       {/* 他のコンポーネントやコンテンツ */}
     </div>
   );
---
+++ src/components/about/about.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './about.css';
+
+const About: React.FC = () => {
+  return (
+    <section className="about">
+      <div className="about-content">
+        <img src="/img/78238.jpg" alt="プロフィール写真" className="profile-pic" />
+        <div className="info">
+          <h2 className="name">名前: 竹内 快</h2>
+          <p className="age">年齢: 24歳</p>
+          <p className="affiliation">所属: 立教大学大学院 人工知能科学研究科 人工知能科学専攻</p>
+          <p className="hobbies">趣味: ゲーム（原神）、漫画を読むこと</p>
+          <p className="effort">最近の取り組み: 自主制作アプリの改修、競技プログラミング、モダンな技術の習得</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
