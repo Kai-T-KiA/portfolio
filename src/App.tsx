698f1166a7049f2b09b7abf711a5387210c8aba9
竹内 快 <>
2024-09-18 19:04:19 +0900

    career.tsxの作成と表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from './components/header/header';
 import About from './components/about/about';
+import Career from './components/career/career';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
       <About />
+      <Career />
       {/* 他のコンポーネントやコンテンツ */}
     </div>
   );
